750f31ed76cd42fb57f77e6985be7f458b076936
Stephen Smithwick <stephen.smithwick@gmail.com>
2016-01-14 10:45:54 +1100

    Adding products to the results

--- src/credit-cards/search/product.js
+++ src/credit-cards/search/product.js
@@ -4,87 +4,49 @@ import ReactDOM from 'react-dom';
 var RCProduct = React.createClass({
   render: function() {
     return (
-          <div className='row navigation hidden-xs'>
-            <div className='col-sm-6'>
-              <div className='row'>
-                <div className='col-xs-3'>
-                  <b>Borrow</b>
-                  <ul className='list-unstyled'>
-                    <li><a href="/home-loans">Home Loans Comparison</a></li>
-                    <li><a href="/credit-cards">Credit Cards</a></li>
-                    <li><a href="/personal-loans">Personal Loans</a></li>
-                    <li><a href="/car-loans">Car Loans</a></li>
-                  </ul>
-                </div>
-                <div className='col-xs-3'>
-                  <b>Save</b>
-                  <ul className='list-unstyled'>
-                    <li><a href="/savings-accounts/high-interest">High Interest Savings Accounts</a></li>
-                    <li><a href="/transaction-accounts">Transaction Accounts</a></li>
-                    <li><a href="/savings-accounts">Compare Savings Accounts</a></li>
-                    <li><a href="/term-deposits">Term Deposits</a></li>
-                  </ul>
-                </div>
-                <div className='col-xs-3'>
-                  <b>Invest</b>
-                  <ul className='list-unstyled'>
-                    <li><a href="/superannuation">Superannuation</a></li>
-                    <li><a href="/managed-funds">Managed Funds</a></li>
-                    <li><a href="/superannuation">Pension Funds</a></li>
-                    <li><a href="/home-loans/SMSF-loans">SMSF Loans</a></li>
-                  </ul>
-                </div>
-                <div className='col-xs-3'>
-                  <b>Calculators</b>
-                  <ul className='list-unstyled'>
-                    <li><a href="/home-loans/calculator">Home Loan Calculator</a></li>
-                    <li><a href="/car-loans/calculator">Car Loan Calculator</a></li>
-                    <li><a href="/personal-loans/calculator">Personal Loan Calculator</a></li>
-                    <li><a href="/term-deposits/calculator">Term Deposit Calculator</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className='col-sm-6'>
-              <div className='row'>
-                <div className='col-xs-3'>
-                  <b>Guides</b>
-                  <ul className='list-unstyled'>
-                    <li><a href="/home-loans/guide">Home Loans Guide</a></li>
-                    <li><a href="/credit-cards/guide">Credit Cards Guide</a></li>
-                    <li><a href="/savings-accounts/guide">Savings Accounts Guide</a></li>
-                    <li><a href="/term-deposits/guide">Term Deposit Guides</a></li>
-                  </ul>
-                </div>
-                <div className='col-xs-3'>
-                  <b>News</b>
-                  <ul className='list-unstyled'>
-                    <li><a href="/home-loans/mortgage-news">Home Loans News</a></li>
-                    <li><a href="/credit-cards/news">Credit Cards News</a></li>
-                    <li><a href="/savings-accounts/news">Savings Accounts News</a></li>
-                    <li><a href="/superannuation/news">Superannuation News</a></li>
-                  </ul>
-                </div>
-                <div className='col-xs-3'>
-                  <b>RateCity</b>
-                  <ul className='list-unstyled'>
-                    <li><a href="/about-us">About Us</a></li>
-                    <li><a href="/contact-us">Contact Us</a></li>
-                    <li><a href="/media-room">Media Room</a></li>
-                  </ul>
-                </div>
-                <div className='col-xs-3'>
-                  <b>More Info</b>
-                  <ul className='list-unstyled'>
-                    <li><a href="/financial-dictionary">Financial Dictionary</a></li>
-                    <li><a href="/companies">Companies we compare</a></li>
-                    <li><a href="/how-we-make-money">How we make money</a></li>
-                    <li><a href="/sitemap">Sitemap</a></li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
+      <tr>
+        <td className='text-center hidden-xs'>
+          <div className='btn-compare fa fa-square-o' data-add-url='/credit-cards/comparison/202/add' data-product-id='202' data-remove-url='/credit-cards/comparison/202/remove'>Compare</div>
+        </td>
+        <td className='text-center xs-show-3'>
+          <img className="img-responsive" src="http://placehold.it/88x31" />
+        </td>
+        <td className='xs-show-9 product-name'>
+          <a data-ga-send="event" data-ga-category="Search Result" data-ga-action="Product Title Click" data-ga-label="frank credit card" href="/credit-cards/me-bank/frank-credit-card">frank credit card
+</a></td>
+        <td className='text-center xs-show-3 xs-left'>
+          <img className="img-responsive" src="http://placehold.it/30x30" />
+        </td>
+        <td className='text-center xs-show-3'>
+          <div className='small product-xs-head visible-xs'>Interest Rate</div>
+          <p className='cct-percent'>
+            <strong>
+              9.99%
+            </strong>
+          </p>
+        </td>
+        <td className='text-center xs-show-3'>
+          <div className='small product-xs-head visible-xs'>Annual Fee</div>
+          <strong className='cct-percent'>
+            $0
+          </strong>
+        </td>
+        <td className='text-center xs-show-3'>
+          <div className='small product-xs-head visible-xs'>Max Free Days</div>
+          <strong>
+            55
+          </strong>
+        </td>
+        <td className='text-center hidden-xs'>
+          <strong>
+            $10
+          </strong>
+        </td>
+
+        <td className='text-center xs-show-12'>
+          <a className="btn btn-primary" target="_blank" data-ga-send="event" data-ga-category="Search Result" data-ga-action="Apply Click" data-ga-label="frank credit card" href="/credit-cards/promotions/213/click?filters=&amp;type=goto_site">Go to Site</a>
+        </td>
+      </tr>
     );
   }
 });
--- src/credit-cards/search/results.js
+++ src/credit-cards/search/results.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import RCFilters from './filters';
+import RCProduct from './product';
 
 import elasticsearch from 'elasticsearch';
 
@@ -23,7 +24,6 @@ var RCResults = React.createClass({
       q: 'anz'
     }).then(
       function (body) {
-        console.log("yeah" + self);
         self.setState({ results: body.hits.hits })
       },
       function (error) {
@@ -33,6 +33,10 @@ var RCResults = React.createClass({
   },
 
   render: function() {
+    var products = this.state.results.map(function() {
+      return (<RCProduct/>);
+    });
+    console.log(products);
     return (
         <table className='table table-striped credit-card-table'>
           <tbody>
@@ -53,55 +57,7 @@ var RCResults = React.createClass({
               <th className='text-center' data-container='body' data-content='RateCity may receive remuneration for referrals to these links and/or as a consequence of a consumer acquiring a credit product after following these links.' data-placement='auto' data-toggle='popover'
               data-trigger='hover'>Go To Site</th>
             </tr>
-
-            <div className='products-featured'>
-            </div>
-            <div className='products-search'>
-              <tr>
-                <td className='text-center hidden-xs'>
-                  <div className='btn-compare fa fa-square-o' data-add-url='/credit-cards/comparison/202/add' data-product-id='202' data-remove-url='/credit-cards/comparison/202/remove'>Compare</div>
-                </td>
-                <td className='text-center xs-show-3'>
-                  <img className="img-responsive" src="http://placehold.it/88x31" />
-                </td>
-                <td className='xs-show-9 product-name'>
-                  <a data-ga-send="event" data-ga-category="Search Result" data-ga-action="Product Title Click" data-ga-label="frank credit card" href="/credit-cards/me-bank/frank-credit-card">frank credit card
-    </a></td>
-                <td className='text-center xs-show-3 xs-left'>
-                  <img className="img-responsive" src="http://placehold.it/30x30" />
-                </td>
-                <td className='text-center xs-show-3'>
-                  <div className='small product-xs-head visible-xs'>Interest Rate</div>
-                  <p className='cct-percent'>
-                    <strong>
-                      9.99%
-                    </strong>
-                  </p>
-                </td>
-                <td className='text-center xs-show-3'>
-                  <div className='small product-xs-head visible-xs'>Annual Fee</div>
-                  <strong className='cct-percent'>
-                    $0
-                  </strong>
-                </td>
-                <td className='text-center xs-show-3'>
-                  <div className='small product-xs-head visible-xs'>Max Free Days</div>
-                  <strong>
-                    55
-                  </strong>
-                </td>
-                <td className='text-center hidden-xs'>
-                  <strong>
-                    $10
-                  </strong>
-                </td>
-
-                <td className='text-center xs-show-12'>
-                  <a className="btn btn-primary" target="_blank" data-ga-send="event" data-ga-category="Search Result" data-ga-action="Apply Click" data-ga-label="frank credit card" href="/credit-cards/promotions/213/click?filters=&amp;type=goto_site">Go to Site</a>
-                </td>
-              </tr>
-
-            </div>
+            {products}
           </tbody>
         </table>
     );
