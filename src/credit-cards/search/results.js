a7f1465dd0a9b91f62405a9a6cec0afee02e58ad
Stephen Smithwick <stephen.smithwick@gmail.com>
2016-01-14 11:39:16 +1100

    This query works

--- src/credit-cards/search/results.js
+++ src/credit-cards/search/results.js
@@ -20,9 +20,15 @@ var RCResults = React.createClass({
 
   componentWillMount: function() {
     var self = this;
+    var query = {
+      "size":25,
+      "query":{"function_score":{"query":{"filtered":{"filter":[{"bool":{"must":[{"query":{"query_string":{"query":"buckets:overseas_spending"}}},{"term":{"visible":true}},{"term":{"is_business":false}},{"term":{"is_secure":false}}]}}]}},"functions":[{"filter":{"bool":{"must":{"term":{"promotions.regions":"promoted_search"}}}},"weight":2}]}},"aggregations":{"bucket_rewards":{"filter":{"query":{"query_string":{"query":"buckets:rewards"}}}},"bucket_low_fees":{"filter":{"query":{"query_string":{"query":"buckets:low_fees"}}}},"bucket_low_rates":{"filter":{"query":{"query_string":{"query":"buckets:low_rates"}}}},"bucket_all_offers":{"filter":{"query":{"query_string":{"query":"buckets:all_offers"}}}},"bucket_intro_offers":{"filter":{"query":{"query_string":{"query":"buckets:intro_offers"}}}},"bucket_bt_intro_offers":{"filter":{"query":{"query_string":{"query":"buckets:bt_intro_offers"}}}},"bucket_special_offers":{"filter":{"query":{"query_string":{"query":"buckets:special_offers"}}}},"bucket_overseas_spending":{"filter":{"query":{"query_string":{"query":"buckets:overseas_spending"}}}},"bucket_frequent_flyer":{"filter":{"query":{"query_string":{"query":"buckets:frequent_flyer"}}}},"bucket_big_four":{"filter":{"query":{"query_string":{"query":"buckets:big_four"}}}},"bucket_perks":{"filter":{"query":{"query_string":{"query":"buckets:perks"}}}},"companies":{"terms":{"field":"company_id","size":10}},"card_types":{"terms":{"field":"card_type","size":4,"min_doc_count":0}}},
+      "sort":[{"_score":{"order":"desc"}},{"international_withdrawal_cost":{"order":"asc"}},{"currency_conversion_fee":{"order":"asc"}},{"annual_fee_currency":{"order":"asc"}}]
+    }
     client.search({
      "index": "credit_cards-products",
-     "body": {"size":25,"query":{"function_score":{"query":{"filtered":{"filter":[{"bool":{"must":[{"term":{"visible":true}},{"term":{"is_business":false}},{"term":{"is_secure":false}}]}}]}}}},"aggregations":{"bucket_rewards":{"filter":{"query":{"query_string":{"query":"buckets:rewards"}}}},"bucket_low_fees":{"filter":{"query":{"query_string":{"query":"buckets:low_fees"}}}},"bucket_low_rates":{"filter":{"query":{"query_string":{"query":"buckets:low_rates"}}}},"bucket_all_offers":{"filter":{"query":{"query_string":{"query":"buckets:all_offers"}}}},"bucket_intro_offers":{"filter":{"query":{"query_string":{"query":"buckets:intro_offers"}}}},"bucket_bt_intro_offers":{"filter":{"query":{"query_string":{"query":"buckets:bt_intro_offers"}}}},"bucket_special_offers":{"filter":{"query":{"query_string":{"query":"buckets:special_offers"}}}},"bucket_overseas_spending":{"filter":{"query":{"query_string":{"query":"buckets:overseas_spending"}}}},"bucket_frequent_flyer":{"filter":{"query":{"query_string":{"query":"buckets:frequent_flyer"}}}},"bucket_big_four":{"filter":{"query":{"query_string":{"query":"buckets:big_four"}}}},"bucket_perks":{"filter":{"query":{"query_string":{"query":"buckets:perks"}}}},"companies":{"terms":{"field":"company_id","size":10}},"card_types":{"terms":{"field":"card_type","size":4,"min_doc_count":0}}},"sort":[{"_score":{"order":"desc"}},{"purchase_standard_rate_percentage":{"order":"asc"}},{"annual_fee_currency":{"order":"asc"}},{"interest_free_period_quantity":{"order":"desc"}}]}
+     "type": "product",
+     "body": query
     }).then(
       function (body) {
         self.setState({ results: body.hits.hits })
@@ -35,6 +41,7 @@ var RCResults = React.createClass({
 
   render: function() {
     var products = this.state.results.map(function(product) {
+      console.log(product);
       return (<RCProduct name={product._source.name}/>);
     });
     console.log(products);
