2a7b0d2246f3db2e90b2eb3b194b93e8faedbe87
Stephen Smithwick <stephen.smithwick@gmail.com>
2016-01-14 11:26:27 +1100

    Updating the elastic search query to use a real one from the app

--- src/credit-cards/search/results.js
+++ src/credit-cards/search/results.js
@@ -7,7 +7,7 @@ import RCProduct from './product';
 import elasticsearch from 'elasticsearch';
 
 var client = new elasticsearch.Client({
-  host: 'http://search-dev-ratecity01-xxqfhvgnouqfnppttlgd3wu4du.ap-southeast-2.es.amazonaws.com/credit_cards-products/',
+  host: 'http://search-dev-ratecity01-xxqfhvgnouqfnppttlgd3wu4du.ap-southeast-2.es.amazonaws.com',
   log: 'trace'
 });
 
@@ -21,7 +21,8 @@ var RCResults = React.createClass({
   componentWillMount: function() {
     var self = this;
     client.search({
-      q: 'anz'
+     "index": "credit_cards-products",
+     "body": {"size":25,"query":{"function_score":{"query":{"filtered":{"filter":[{"bool":{"must":[{"term":{"visible":true}},{"term":{"is_business":false}},{"term":{"is_secure":false}}]}}]}}}},"aggregations":{"bucket_rewards":{"filter":{"query":{"query_string":{"query":"buckets:rewards"}}}},"bucket_low_fees":{"filter":{"query":{"query_string":{"query":"buckets:low_fees"}}}},"bucket_low_rates":{"filter":{"query":{"query_string":{"query":"buckets:low_rates"}}}},"bucket_all_offers":{"filter":{"query":{"query_string":{"query":"buckets:all_offers"}}}},"bucket_intro_offers":{"filter":{"query":{"query_string":{"query":"buckets:intro_offers"}}}},"bucket_bt_intro_offers":{"filter":{"query":{"query_string":{"query":"buckets:bt_intro_offers"}}}},"bucket_special_offers":{"filter":{"query":{"query_string":{"query":"buckets:special_offers"}}}},"bucket_overseas_spending":{"filter":{"query":{"query_string":{"query":"buckets:overseas_spending"}}}},"bucket_frequent_flyer":{"filter":{"query":{"query_string":{"query":"buckets:frequent_flyer"}}}},"bucket_big_four":{"filter":{"query":{"query_string":{"query":"buckets:big_four"}}}},"bucket_perks":{"filter":{"query":{"query_string":{"query":"buckets:perks"}}}},"companies":{"terms":{"field":"company_id","size":10}},"card_types":{"terms":{"field":"card_type","size":4,"min_doc_count":0}}},"sort":[{"_score":{"order":"desc"}},{"purchase_standard_rate_percentage":{"order":"asc"}},{"annual_fee_currency":{"order":"asc"}},{"interest_free_period_quantity":{"order":"desc"}}]}
     }).then(
       function (body) {
         self.setState({ results: body.hits.hits })
